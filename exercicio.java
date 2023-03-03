ec804f1e964401a1f9936aee51204f58d21d70c6
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-03 10:07:41 -0300

    declarando e somando variáveis com java

---
+++ exercicio.java
@@ -0,0 +1,10 @@
+public class exercicio {
+    public static void main(String[] args){
+        int num = 3, numDois = 3, soma;
+        soma = num + numDois;
+        System.out.println(soma);
+
+    }
+
+
+}
