c2e277d7fd5c953e61f817d95e5f495d8193feeb
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-03 11:13:37 -0300

    treinando variaveis e constantes transformando graus

---
+++ CelsiusParaFahrenheit.java
@@ -0,0 +1,11 @@
+public class CelsiusParaFahrenheit {
+    public static void main(String[] args){
+        //(0 °C × 9/5) + 32
+        double celsius = 27;
+        final double divisao = 9/5;
+        final int tempBase = 32;
+
+
+        System.out.println((celsius * 9/5) + tempBase);
+    }
+}
