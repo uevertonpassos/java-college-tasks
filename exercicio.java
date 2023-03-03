579e88c61e82111ca83ac172b94bb96c57307265
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-03 10:17:07 -0300

    declarando e somando variáveis com java e aprendendo a criar variáveis que não alteram o seu valor no final usando o método "final"

--- exercicio.java
+++ exercicio.java
@@ -1,8 +1,11 @@
 public class exercicio {
     public static void main(String[] args){
         int num = 3, numDois = 3, soma;
+        final String batatas = "batatas assadas";
+
         soma = num + numDois;
         System.out.println(soma);
+        System.out.println(batatas);
 
     }
 
