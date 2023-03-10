da55a5c32a54ab800584a94f64eb404d9b7ffd21
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-10 11:43:37 -0300

    declaração de variáveis

---
+++ primitiveTypes.java
@@ -0,0 +1,42 @@
+public class{
+    public static void main(String[] args){
+        //forma normal de se declarar uma variável tipo inteiro
+        int num  = 3; 
+        //usando typecast para declarar uma variável
+        int idade = (int) 3;
+        // usando wrapper para declarar uma variável
+        Integer idade = new Integer(3);
+
+        //forma normal de se declarar uma variável tipo float;
+        float sal = 1825.54f;
+
+        //usando typecast para de clarar uma variável tipo float
+        float sal = (float) 1825.54;
+
+        //usando wrapper para declarar uma variável tipo float
+        Float sal = new Float (1825.54);
+
+        //forma normal de declarar uma variável tipo char
+        char letra = 'G';
+        
+        //usando typecast para declarar uma variável tipo char
+        char letra = (char) 'G';
+
+        //usando wrapper para declarar uma variável tipo char
+        Character letra = new Character ('G');
+
+       //forma normal de declarar uma variável tipo boolean 
+       boolean casado = false; 
+
+       //usando typecast para declarar uma variável tipo boolean
+       boolean casado = (boolean) false;
+
+       //usando wrapper para declarar uma variável tipo boolean
+
+       Boolean casado = new Boolean(false);
+       
+
+
+
+    }
+}
