8535b64485b34dc4b0fa8d660b91e721c51fb97b
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-14 12:07:58 -0300

    treinando

---
+++ CelsiusParaFahrenheitDesafio.java
@@ -0,0 +1,12 @@
+public class CelsiusParaFahrenheitDesafio {
+    public static void main(String[] args){
+        final double FATOR = 9.0/5.0;
+        final int ADD = 32;
+        int celsius = 27;
+        double fahrenheit = (celsius * FATOR) + ADD;
+
+        System.out.println("O resultado sera: " + fahrenheit);
+
+
+    }
+}
